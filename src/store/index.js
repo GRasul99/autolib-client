911004b09997ec2e7e30e6d3879cdb9254daddb2
GRasul99 <rasulganiyev1999@gmail.com>
2020-08-09 23:06:30 +0500

    Removed language module from store

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,6 @@ import * as category from '@/store/modules/category'
 import * as auth from '@/store/modules/auth'
 import * as university from '@/store/modules/university'
 import * as role from '@/store/modules/role.js'
-import * as lang from '@/store/modules/lang.js'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -14,7 +13,6 @@ export default new Vuex.Store({
     category,
     auth,
     university,
-    role,
-    lang
+    role
   }
 })
